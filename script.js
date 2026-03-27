7c5ff70ac0387bff5c9cee2914cc3cb39a10af17
DuskoKovac <duledusko021@gmail.com>
2026-03-27 01:27:39 +0100

    radjen paralel fetch

--- script.js
+++ script.js
@@ -3,75 +3,19 @@ var selectOptionEl = document.getElementById("themes");
 var smallImgElements = document.getElementsByClassName("smallImg");
 var changeSmallEl = document.getElementById("changeSmall");
 var imageDivEl = document.getElementById("imageDiv");
-// var changeBackgroundEl = document.getElementById("changeBackground");
+
 var bodyClass = bodyEl.getAttribute("class");
-var cardTextEl1 = document.getElementById("cardText-1");
-var cardTextEl2 = document.getElementById("cardText-2");
-var cardTextEl3 = document.getElementById("cardText-3");
-var textJokeEl = document.getElementById("textJoke");
+// var cardTextEl1 = document.getElementById("cardText-1");
+// var cardTextEl2 = document.getElementById("cardText-2");
+// var cardTextEl3 = document.getElementById("cardText-3");
+var textJokeElements = document.getElementsByClassName("textJoke");
 var changeTextEl = document.getElementById("changeText");
-var textUrl = "https://api.chucknorris.io/jokes/random";
-
-
-async function changeJoke() {
-    var response1 = await fetch(textUrl);
-    var data1 = await response1.json();
-    var response2 = await fetch(textUrl);
-    var data2 = await response2.json();
-    var response3 = await fetch(textUrl);
-    var data3 = await response3.json();
-    var response4 = await fetch(textUrl);
-    var data4 = await response4.json();
-    cardTextEl1.innerHTML = "";
-    cardTextEl2.innerHTML = "";
-    cardTextEl3.innerHTML = "";
-    textJokeEl.innerHTML = "";
-    var txt1 = `
-            ${data1.value}
-        `
-    var txt2 = `
-            ${data2.value}
-        `
-    var txt3 = `
-            ${data3.value}
-        `
-    var txt4 = `
-            ${data3.value}
-      `
-    cardTextEl1.innerHTML = txt1;
-    cardTextEl2.innerHTML = txt2;
-    cardTextEl3.innerHTML = txt3;
-    textJokeEl.innerHTML = txt4;
-}
-
-async function changeJoke2() {
-    var response = await fetch(textUrl2);
-    var data = await response.json();
-
-    textJokeEl.innerHTML = "";
-
-    var txt1 = `
-            ${data1.value}
-    `
-    var txt2 = `
-            ${data2.value}
-    `
-    var txt3 = `
-            ${data3.value}
-    `
-    cardTextEl1.innerHTML = txt1;
-    cardTextEl2.innerHTML = txt2;
-    cardTextEl3.innerHTML = txt3;
-}
-
-changeTextEl.addEventListener("click", function () {
-    changeJoke();
-
-});
-
-
-
-
+var textUrlList = [
+    "https://api.chucknorris.io/jokes/random",
+    "https://api.chucknorris.io/jokes/random",
+    "https://api.chucknorris.io/jokes/random",
+    "https://api.chucknorris.io/jokes/random"
+]
 var images = [`../ img / ${bodyClass}/1.jpg`,
 `../img/${bodyClass}/2.jpg`,
 `../img/${bodyClass}/3.jpg`,
@@ -89,9 +33,50 @@ var images = [`../ img / ${bodyClass}/1.jpg`,
 `../img/${bodyClass}/15.jpg`
 ];
 
+
+async function getUrl(url) {
+    var response = await fetch(url);
+    var data = await response.json();
+    return data;
+}
+
+async function getAllUrls(allUrls) {
+    var promises = allUrls.map(function (url) {
+        return getUrl(url);
+    });
+    var results = await Promise.all(promises);
+    for (i = 0; i < textJokeElements.length; i++) {
+
+        // for (j = 0; j < results.length; j++) {
+        // if (i === j) {
+        textJokeElements[i].innerHTML = results[i].value
+        console.log(textJokeElements[i], results[i].value);
+        // }
+        // }
+    }
+
+
+
+
+}
+
+
+
+
+
+changeTextEl.addEventListener("click", function () {
+    getAllUrls(textUrlList);
+
+});
+
+
+
+
+
+
 window.addEventListener("load", function () {
     randomize(images, bodyClass);
-    changeJoke();
+    getAllUrls(textUrlList);
 });
 
 selectOptionEl.addEventListener("change", function (event) {
@@ -100,7 +85,7 @@ selectOptionEl.addEventListener("change", function (event) {
 
     bodyEl.setAttribute("class", selectedTheme);
     randomize(images, bodyClass);
-    changeJoke();
+
 });
 
 changeSmallEl.addEventListener("click", function () {
@@ -108,37 +93,44 @@ changeSmallEl.addEventListener("click", function () {
     randomize(images, bodyClass);
 });
 
+
+function getImages(value) {
+    return ([`../img/${value}/1.jpg`,
+    `../img/${value}/2.jpg`,
+    `../img/${value}/3.jpg`,
+    `../img/${value}/4.jpg`,
+    `../img/${value}/5.jpg`,
+    `../img/${value}/6.jpg`,
+    `../img/${value}/7.jpg`,
+    `../img/${value}/8.jpg`,
+    `../img/${value}/9.jpg`,
+    `../img/${value}/10.jpg`,
+    `../img/${value}/11.jpg`,
+    `../img/${value}/12.jpg`,
+    `../img/${value}/13.jpg`,
+    `../img/${value}/14.jpg`,
+    `../img/${value}/15.jpg`
+    ]);
+
+}
+
 function randomize(array, theme) {
 
     theme = bodyEl.getAttribute("class");
 
-    var array = [`../img/${theme}/1.jpg`,
-    `../img/${theme}/2.jpg`,
-    `../img/${theme}/3.jpg`,
-    `../img/${theme}/4.jpg`,
-    `../img/${theme}/5.jpg`,
-    `../img/${theme}/6.jpg`,
-    `../img/${theme}/7.jpg`,
-    `../img/${theme}/8.jpg`,
-    `../img/${theme}/9.jpg`,
-    `../img/${theme}/10.jpg`,
-    `../img/${theme}/11.jpg`,
-    `../img/${theme}/12.jpg`,
-    `../img/${theme}/13.jpg`,
-    `../img/${theme}/14.jpg`,
-    `../img/${theme}/15.jpg`
-    ];
+    var images = getImages(theme);
+    // console.log(images);
 
-    for (var i = array.length - 1; i > 0; i--) {
+    for (var i = images.length - 1; i > 0; i--) {
         var j = Math.floor(Math.random() * (i + 1));
-        var k = array[i];
-        array[i] = array[j];
-        array[j] = k;
+        var k = images[i];
+        images[i] = images[j];
+        images[j] = k;
     }
 
     for (i = 0; i < smallImgElements.length; i++) {
 
-        smallImgElements[i].setAttribute("src", array[i]);
+        smallImgElements[i].setAttribute("src", images[i]);
     }
 }
 
